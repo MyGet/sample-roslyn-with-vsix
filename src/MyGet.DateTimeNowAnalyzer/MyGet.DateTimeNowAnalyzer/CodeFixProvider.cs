a8c9d328747fb37fe34fd580a841a5725dc3f465
Maarten Balliauw <maarten@maartenballiauw.be>
2015-05-21 09:48:49 +0200

    Removed unused usings

--- src/MyGet.DateTimeNowAnalyzer/MyGet.DateTimeNowAnalyzer/CodeFixProvider.cs
+++ src/MyGet.DateTimeNowAnalyzer/MyGet.DateTimeNowAnalyzer/CodeFixProvider.cs
@@ -9,10 +9,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
 
 namespace MyGet.DateTimeNowAnalyzer
 {
